9fdb66328ac743bfe4d37d0034291865b6d5f9e8
Wildrimak <finalwildrimak@gmail.com>
2019-12-17 18:39:26 -0300

    Teste feito

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -4,14 +4,10 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.alura.forum.controller.form.TopicoForm;
 import br.com.alura.forum.dto.TopicoDto;
 import br.com.alura.forum.modelo.Topico;
-import br.com.alura.forum.repository.CursoRepository;
 import br.com.alura.forum.repository.TopicoRepository;
 
 @RestController("/topicos")
@@ -20,20 +16,22 @@ public class TopicosController {
 	@Autowired
 	private TopicoRepository topicoRepository;
 	
-	@Autowired
-	private CursoRepository cursoRepository;
+	//@Autowired
+	//private CursoRepository cursoRepository;
 
 	@GetMapping
 	public List<TopicoDto> lista() {
 
+		System.out.println("CARREGANDO...");
 		List<Topico> topicos = topicoRepository.findAll();
+		System.out.println(topicos);
 		return TopicoDto.converter(topicos);
 	}
-	
+	/*
 	@PostMapping
 	public void cadastrar(@RequestBody TopicoForm topicoForm) {
 		Topico topico = topicoForm.converter(cursoRepository);
 		topicoRepository.save(topico);
 	}
-
+	*/
 }
