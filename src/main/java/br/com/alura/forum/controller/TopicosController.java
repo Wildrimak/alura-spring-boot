16a5b1a2485922e568716139a3746c8de28588b9
Wildrimak <finalwildrimak@gmail.com>
2019-12-17 18:17:45 -0300

    tratando post

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -1,12 +1,15 @@
 package br.com.alura.forum.controller;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import br.com.alura.forum.controller.form.TopicoForm;
 import br.com.alura.forum.dto.TopicoDto;
@@ -20,8 +23,8 @@ public class TopicosController {
 	@Autowired
 	private TopicoRepository topicoRepository;
 	
-	@Autowired
-	private CursoRepository cursoRepository;
+	//@Autowired
+	//private CursoRepository cursoRepository;
 
 	@GetMapping
 	public List<TopicoDto> lista() {
@@ -30,10 +33,13 @@ public class TopicosController {
 		return TopicoDto.converter(topicos);
 	}
 	
+	/*		
 	@PostMapping
-	public void cadastrar(@RequestBody TopicoForm topicoForm) {
+	public ResponseEntity<TopicoDto> cadastrar(@RequestBody TopicoForm topicoForm, UriComponentsBuilder uriBuilder) {
 		Topico topico = topicoForm.converter(cursoRepository);
 		topicoRepository.save(topico);
+		URI uri = uriBuilder.path("/topicos/{id}").buildAndExpand(topico.getId()).toUri();
+		return ResponseEntity.created(uri).body(new TopicoDto(topico));
 	}
-
+*/
 }
